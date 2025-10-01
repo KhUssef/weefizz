d819c33a1f59c7ba9940593fc0b5e2bef8b92e92
KhUssef <khalifayoussef628@gmail.com>
2025-10-01 17:29:58 +0100

    rapport

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,15 +5,15 @@ export default () => ({
   db: {
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT || '3306', 10),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME || 'newuser',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE_NAME || 'project',
   },
 
   jwt: {
-    accessSecret: process.env.JWT_ACCESS_SECRET,
+    accessSecret: process.env.JWT_ACCESS_SECRET || 'defaultAccessSecret',
     accessExpires: process.env.JWT_ACCESS_EXPIRES || '15m',
-    refreshSecret: process.env.JWT_REFRESH_SECRET,
+    refreshSecret: process.env.JWT_REFRESH_SECRET || 'defaultRefreshSecret',
     refreshExpires: process.env.JWT_REFRESH_EXPIRES || '7d',
   },
 });
